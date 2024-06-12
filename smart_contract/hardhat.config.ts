e22b270ed9a51256225b55cbc5074da8863aa6ee
shaunmsoria <shaunmsoria@gmail.com>
2024-06-13 08:24:40 +1000

    working on the smart contract deployment

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -6,9 +6,17 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
+    hardhat2: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/kDsQmG9DLRQC6_Ysaah0tPKqo3bZFAKw`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
     hardhat: {
       forking: {
-        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       }
     }
   }
