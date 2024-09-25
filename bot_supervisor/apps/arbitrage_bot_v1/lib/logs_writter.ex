37ffb049b71c6655609791f6b8cdd7961a92db5b
shaunmsoria <shaunmsoria@gmail.com>
2024-09-25 19:35:43 +1000

    implemented and tested 1st version of LogsWritter

--- bot_supervisor/apps/arbitrage_bot_v1/lib/logs_writter.ex
+++ bot_supervisor/apps/arbitrage_bot_v1/lib/logs_writter.ex
@@ -3,8 +3,8 @@ defmodule LogsWritter do
 
   # Client API
 
-  def start_link([]) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(%{}) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
   def save_logs do
@@ -13,60 +13,40 @@ defmodule LogsWritter do
 
   # GenServer callbacks
 
-  def init([]) do
+  def init(%{}) do
     GenServer.cast(__MODULE__, :save_logs)
 
-    {:ok, []}
+    {:ok, %{}}
   end
 
   def handle_cast(:save_logs, state) do
-    #TODO open a file with the name being today's data
-    #TODO write the content of the logs concache at the end of the file (append)
-    #TODO then reset state of concache logs
-    #TODO recall LogsWritter after 1min
+    # TODO open a file with the name being today's data
+    # TODO write the content of the logs concache at the end of the file (append)
+    # TODO then reset state of concache logs
+    # TODO recall LogsWritter after 1min
 
-    # gas_result = EtherscanGasTrackerApi.get_gas_oracle()
+    IO.puts("sx1 in save_logs")
 
-    # case gas_result do
-    #   %{
-    #     "result" => %{
-    #       "FastGasPrice" => fast_gas_price_raw,
-    #       "LastBlock" => last_block
-    #     }
-    #   } ->
-    #     max_gas_limit =
-    #       convert_string_to_value(System.get_env("GAS_LIMIT"))
+    # ConCache.put(:logs, :console, "test\n")
 
-    #     fast_gas_price =
-    #       convert_string_to_value(fast_gas_price_raw)
+    message = ConCache.get(:logs, :console)
+    |> IO.inspect(label: "sx1 logs console before the concache get")
 
-    #     estimated_gas_fee = fast_gas_price * max_gas_limit / 1_000_000_000
+    date = Date.utc_today()
 
-    #     ConCache.put(:gas, :fast_gas_price, fast_gas_price)
-    #     ConCache.put(:gas, :last_block, last_block)
-    #     ConCache.put(:gas, :estimated_gas_fee, estimated_gas_fee)
+    file_name = "console_#{date.day}_#{date.month}_#{date.year}.log"
 
-    #   {:error, reason} ->
-    #     %{"error" => reason} |> IO.inspect(label: "sx1 gas_extract error result")
-    # end
+    File.write("/home/shaun/Programs/sab/bot_supervisor/log/#{file_name}", message, [:append])
 
-    # :timer.sleep(1000)
-    # GenServer.cast(__MODULE__, :save_logs)
+
+
+    :timer.sleep(1000)
+
+    ConCache.put(:logs, :console, "")
+
+
+    GenServer.cast(__MODULE__, :save_logs)
 
     {:noreply, state}
   end
-
-  # def convert_string_to_value(number) when is_binary(number) do
-  #   with number_list <- String.split(number, "."),
-  #        true <- is_number_list_an_integer?(number_list) do
-  #     number |> String.to_integer()
-  #   else
-  #     _ ->
-  #       number |> String.to_float()
-  #   end
-  # end
-
-  # def is_number_list_an_integer?(number_list) do
-  #   length(number_list) == 1
-  # end
 end
