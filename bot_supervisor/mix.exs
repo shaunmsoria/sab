d1f738188286d25f597e50c801b1ae4da7e5764a
shaunmsoria <shaunmsoria@gmail.com>
2024-11-17 17:36:09 +1100

    removed from branch unnecessary deps

--- bot_supervisor/mix.exs
+++ bot_supervisor/mix.exs
@@ -25,8 +25,6 @@ defmodule BotSupervisor.MixProject do
       {:plug, "~> 1.11"},
       {:logger_file_backend, "~> 0.0.14"},
       {:plug_cowboy, "~> 2.5"},
-      {:ecto_sql, "~> 3.2"},
-      {:postgrex, "~> 0.15"}
     ]
   end
 
