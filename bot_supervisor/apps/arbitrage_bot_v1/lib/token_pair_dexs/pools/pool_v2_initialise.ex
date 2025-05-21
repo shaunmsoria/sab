00f7121014231b8c8ea713ee3c31685336a6c4a3
shaunmsoria <shaunmsoria@gmail.com>
2025-05-21 19:15:03 +1000

    saving upstream main updates

--- bot_supervisor/apps/arbitrage_bot_v1/lib/token_pair_dexs/pools/pool_v2_initialise.ex
+++ bot_supervisor/apps/arbitrage_bot_v1/lib/token_pair_dexs/pools/pool_v2_initialise.ex
@@ -101,6 +101,7 @@ defmodule PoolV2Initialise do
   def get_or_create_pair_for_dex(%Dex{name: dex_name, factory: dex_factory} = dex, n_pair) do
     with {:ok, pair_address} <-
            get_all_pairs(dex_factory, n_pair) |> IO.inspect(label: "sx1 get_all_pairs"),
+           true <- String.valid?(pair_address),
          {:ok, pool_address, token0, token1} <-
            get_or_create_pool_address_token0_token1_from_event_address(pair_address, "uniswapV2"),
          {:ok, price, reserve0, reserve1} <-
