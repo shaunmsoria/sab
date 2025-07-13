7761feb5d9cf7b4c886baf0e4e5f884a654bd5d6
shaunmsoria <shaunmsoria@gmail.com>
2025-07-13 11:34:21 +1000

    merge with cross/v2-v3

--- bot_supervisor/apps/arbitrage_bot_v1/lib/token_pair_dexs/pools/pool_v2_initialise.ex
+++ bot_supervisor/apps/arbitrage_bot_v1/lib/token_pair_dexs/pools/pool_v2_initialise.ex
@@ -17,7 +17,7 @@ defmodule PoolV2Initialise do
   # remove comment in get_pairs_for_dex to allow the system to update for all token_pairs
 
   def run() do
-    Repo.update_all(Pool, set: [refresh_reserve: true])
+    # Repo.update_all(Pool, set: [refresh_reserve: true])
 
     with list_dexs_v2 <- DS.with_abi("uniswapV2") |> Repo.all(),
          {:ok, list_dex_token_pairs_length_updated} <- get_all_token_pairs_length(list_dexs_v2) do
